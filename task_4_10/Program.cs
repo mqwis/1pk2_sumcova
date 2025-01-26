dbe4bbcc3c1a537c8c449ad57ccac0f7d654d37c
sumcovamqwis@hotmail.com <sumcovamqwis@hotmail.com>
2025-01-26 20:53:14 +0500

    .

--- task_4_10/Program.cs
+++ task_4_10/Program.cs
@@ -5,31 +5,31 @@ class Program
     static void Main()
     {
         Random rand = new Random();
-        int[] array = new int[10]; //массив на 10 чисел
+        int[] array = new int[10]; // массив на 11 чисел
 
         for (int i = 0; i < array.Length; i++)
         {
-            array[i] = rand.Next(-10, 11); //цикл
+            array[i] = rand.Next(-10, 11); // цикл
         }
         
-        Console.WriteLine("Исходный массив:");
+        Console.WriteLine("Исходный массив:"); // вводим текст в консоль
         for (int i = 0; i < array.Length; i++)
         {
-            Console.Write(array[i] + " ");
+            Console.Write(array[i] + " "); // цикл 
         }
         Console.WriteLine();
 
         for (int i = 0; i < array.Length / 4; i++)
         {
             int temp = array[i];
-            array[i] = array[array.Length / 2 - 1 - i];
+            array[i] = array[array.Length / 2 - 1 - i]; // цикл 
             array[array.Length / 2 - 1 - i] = temp;
         }
 
         for (int i = array.Length / 2; i < array.Length / 2 + array.Length / 4; i++)
         {
             int temp = array[i];
-            array[i] = array[array.Length - 1 - (i - array.Length / 2)];
+            array[i] = array[array.Length - 1 - (i - array.Length / 2)]; // цикл
             array[array.Length - 1 - (i - array.Length / 2)] = temp;
         }
 
